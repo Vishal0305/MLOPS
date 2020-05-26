774f76a28469d34801583da202151a95d145443c
Vishal0305 <64467887+Vishal0305@users.noreply.github.com>
2020-05-26 06:31:46 +0530

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,38 +1,36 @@
+import keras
 from keras.datasets import mnist
 from keras.models import Sequential
+from keras.utils.np_utils import to_categorical
 from keras.layers import Dense
-from keras.utils import np_utils
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-# flatten 28*28 images to a 784 vector for each image
-num_pixels = X_train.shape[1] * X_train.shape[2]
-X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
-X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
-# one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
-# define baseline model
-def baseline_model():
-	# create model
-	model = Sequential()
-	model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
-	model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
-# build the model
-model = baseline_model()
-# Fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=200, verbose=0)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy:  %.2f%%" % (scores[1]*100))
+from keras.optimizers import Adam
+from keras.backend import clear_session
+import numpy
 
-model.save("model/mnist_model.h5")
+# Load Model 
+(train_X , train_y), (test_X , test_y) = mnist.load_data("mymnist.data")
+# Reshape data and change type
+test_X = test_X.reshape(-1 , 28*28)
+train_X = train_X.reshape(-1 ,  28*28)
+test_X = test_X.astype("float32")
+train_X = train_X.astype("float32")
+# One hot encoding 
+test_y = to_categorical(test_y)
+train_y = to_categorical(train_y)
 
-file1 = open("result.txt","w")
-file1.write(str(scores[1]*100))
+model = Sequential()
+
+
+
+model.add(Dense(units=10 , input_dim = 28*28 , activation = 'softmax'))
+model.compile( optimizer= "Adam" , loss='categorical_crossentropy', 
+             metrics=['accuracy'] )
+fit_model = model.fit(train_X ,  train_y , epochs = 2 , verbose =  False)
+
+text = fit_model.history
+accuracy = text['accuracy'][1] * 100
+accuracy = int(accuracy)
+f= open("accuracy.txt","w+")
+f.write(str(accuracy))
+f.close()
+print("Accuracy for the model is : " , accuracy ,"%")
